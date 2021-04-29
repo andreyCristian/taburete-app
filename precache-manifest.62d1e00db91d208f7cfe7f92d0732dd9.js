500cd351c4d1c7224f4bd513704c75826e2bde81
cristian.sossa <cristian.sossa@masivian.com>
2021-04-29 15:48:23 -0500

    initial commit

---
+++ precache-manifest.62d1e00db91d208f7cfe7f92d0732dd9.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84960aa16bc3384f43d",
+    "url": "/taburete-app/css/app.a461fcad.css"
+  },
+  {
+    "revision": "5b4dc5843081da02deb0",
+    "url": "/taburete-app/css/chunk-vendors.77fc7fd4.css"
+  },
+  {
+    "revision": "099a9556e1a63ece24f8a99859c94c7d",
+    "url": "/taburete-app/fonts/fa-brands-400.099a9556.woff"
+  },
+  {
+    "revision": "30cc681d4487d2f561035ba24a68c629",
+    "url": "/taburete-app/fonts/fa-brands-400.30cc681d.eot"
+  },
+  {
+    "revision": "3b89dd103490708d19a95adcae52210e",
+    "url": "/taburete-app/fonts/fa-brands-400.3b89dd10.ttf"
+  },
+  {
+    "revision": "f7307680c7fe85959f3ecf122493ea7d",
+    "url": "/taburete-app/fonts/fa-brands-400.f7307680.woff2"
+  },
+  {
+    "revision": "1f77739ca9ff2188b539c36f30ffa2be",
+    "url": "/taburete-app/fonts/fa-regular-400.1f77739c.ttf"
+  },
+  {
+    "revision": "7124eb50fc8227c78269f2d995637ff5",
+    "url": "/taburete-app/fonts/fa-regular-400.7124eb50.woff"
+  },
+  {
+    "revision": "7630483dd4b0c48639d2ac54a894b450",
+    "url": "/taburete-app/fonts/fa-regular-400.7630483d.eot"
+  },
+  {
+    "revision": "f0f8230116992e521526097a28f54066",
+    "url": "/taburete-app/fonts/fa-regular-400.f0f82301.woff2"
+  },
+  {
+    "revision": "1042e8ca1ce821518a2d3e7055410839",
+    "url": "/taburete-app/fonts/fa-solid-900.1042e8ca.eot"
+  },
+  {
+    "revision": "605ed7926cf39a2ad5ec2d1f9d391d3d",
+    "url": "/taburete-app/fonts/fa-solid-900.605ed792.ttf"
+  },
+  {
+    "revision": "9fe5a17c8ab036d20e6c5ba3fd2ac511",
+    "url": "/taburete-app/fonts/fa-solid-900.9fe5a17c.woff"
+  },
+  {
+    "revision": "e8a427e15cc502bef99cfd722b37ea98",
+    "url": "/taburete-app/fonts/fa-solid-900.e8a427e1.woff2"
+  },
+  {
+    "revision": "ba7ed552362f64d30f6d844974d89114",
+    "url": "/taburete-app/img/fa-brands-400.ba7ed552.svg"
+  },
+  {
+    "revision": "0bb428459c8ecfa61b22a03def1706e6",
+    "url": "/taburete-app/img/fa-regular-400.0bb42845.svg"
+  },
+  {
+    "revision": "376c1f97f6553dea1ca9b3f9081889bd",
+    "url": "/taburete-app/img/fa-solid-900.376c1f97.svg"
+  },
+  {
+    "revision": "228d496349ab45b25192d56f92bf12c2",
+    "url": "/taburete-app/index.html"
+  },
+  {
+    "revision": "e84960aa16bc3384f43d",
+    "url": "/taburete-app/js/app.738e8dad.js"
+  },
+  {
+    "revision": "f7d4c19c1ca20447e38b",
+    "url": "/taburete-app/js/chunk-2d0dd8b8.dc1e752f.js"
+  },
+  {
+    "revision": "2c9ee1357245faf1faaa",
+    "url": "/taburete-app/js/chunk-40f03ee2.c6e35b88.js"
+  },
+  {
+    "revision": "b27c60cd085e06a3b422",
+    "url": "/taburete-app/js/chunk-74679ddc.c26cc874.js"
+  },
+  {
+    "revision": "75aeb3ed93ad261ce17d",
+    "url": "/taburete-app/js/chunk-7a349463.1deb954d.js"
+  },
+  {
+    "revision": "ea49a5847a6ffbc974e2",
+    "url": "/taburete-app/js/chunk-cc600716.05d260bc.js"
+  },
+  {
+    "revision": "5b4dc5843081da02deb0",
+    "url": "/taburete-app/js/chunk-vendors.c8d11a18.js"
+  },
+  {
+    "revision": "1140c5c3cb8225644dc1d2e6b3427fcc",
+    "url": "/taburete-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/taburete-app/robots.txt"
+  }
+]);
